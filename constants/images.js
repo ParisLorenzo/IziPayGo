cdac1816cff28e0fddab602dd614a043dc9c85f0
Julian Quispe <julianmql@hotmail.com>
2024-08-08 21:35:48 -0500

    feat: main image changed to png

--- constants/images.js
+++ constants/images.js
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.png";
 import logo2 from "../assets/logo2.png";
-import portada from "../assets/portada.jpg";
+import portada from "../assets/portada.png";
 import check from "../assets/check.png"
 import camera from '../assets/icons/camera.png';
 export default { logo, logo2, portada, check, camera};
