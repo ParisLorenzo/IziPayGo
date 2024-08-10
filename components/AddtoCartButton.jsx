0e26704f5f6175a8d5796521f06d02ab54e20164
Julian Quispe <julianmql@hotmail.com>
2024-08-10 15:08:49 -0500

    feat: button added w icons

--- components/AddtoCartButton.jsx
+++ components/AddtoCartButton.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';  // Importa el ícono
 
 export default function AddToCartButton() {
   return (
     <TouchableOpacity style={styles.button}>
-      <Text style={styles.buttonText}>Agregar al carrito</Text>
+      <View style={styles.buttonContent}>
+        <Icon name="cart-outline" size={24} color="#fff" style={styles.icon} />
+        <Text style={styles.buttonText}>Agregar al carrito</Text>
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#f28b82',
+    backgroundColor: '#FF4240',
     paddingVertical: 15,
     paddingHorizontal: 40,
     borderRadius: 25,
     marginVertical: 20,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: 10,
   },
   buttonText: {
     color: '#fff',
